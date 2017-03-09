c20eef8d467bdc4293edcf5bbdbc9d546f2145e4
Precious Kindo <lilonaony@gmail.com>
2017-03-09 13:45:01 +0100

    created Node method

--- binarytree/__init__.py
+++ binarytree/__init__.py
@@ -1,10 +1,28 @@
 '''
 Binary Tree 
+March 09, 2017
 '''
 
 class Node():
 	def __init__(self, key, value):
 		self.key = key
 		self.value =  value
-		self.left = None
-		self.right = None
+		self._left = None
+		self._right = None
+
+	@left.setter
+	def left(self, value):
+		self._left = value
+
+	def left(self):
+		return self._left
+
+	@right.setter
+	def right(self, value):
+		self._right = value
+
+	def right(self):
+		return self._right
+
+if __name__ == "__main__":
+	pass
